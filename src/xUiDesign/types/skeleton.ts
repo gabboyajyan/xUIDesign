bddcee8df7d634e628e3b86d4da53da0d15953de
Gabriel Boyajyan <gabriel.boyajyan.94@gmail.com>
2025-05-15 15:43:55 +0400

    ...

--- src/xUiDesign/types/skeleton.ts
+++ src/xUiDesign/types/skeleton.ts
@@ -1,61 +1,62 @@
-import { CSSProperties, ReactNode } from "react";
-import { DefaultProps } from ".";
+import { CSSProperties, ReactNode } from 'react';
+import { DefaultProps } from '.';
 
 declare type widthUnit = number | string;
 
 export interface SkeletonElementProps {
-    className?: string;
-    style?: CSSProperties;
-    size?: 'large' | 'small' | 'default' | number;
-    shape?: 'circle' | 'square' | 'round';
-    active?: boolean;
+  className?: string;
+  style?: CSSProperties;
+  size?: 'large' | 'small' | 'default' | number;
+  shape?: 'circle' | 'square' | 'round';
+  active?: boolean;
 }
 
 export type SkeletonProps = DefaultProps & {
-    active?: boolean;
-    loading?: boolean;
-    className?: string;
-    style?: CSSProperties;
-    children?: ReactNode;
-    avatar?: SkeletonAvatarProps | boolean;
-    title?: SkeletonTitleProps | boolean;
-    paragraph?: SkeletonParagraphProps | boolean;
-    round?: boolean;
-    teamLogo?: boolean;
-}
+  active?: boolean;
+  loading?: boolean;
+  className?: string;
+  style?: CSSProperties;
+  children?: ReactNode;
+  avatar?: SkeletonAvatarProps | boolean;
+  title?: SkeletonTitleProps | boolean;
+  paragraph?: SkeletonParagraphProps | boolean;
+  round?: boolean;
+  teamLogo?: boolean;
+};
 
 interface SkeletonTitleProps {
-    className?: string;
-    style?: CSSProperties;
-    width?: number | string;
+  className?: string;
+  style?: CSSProperties;
+  width?: number | string;
 }
 
 interface SkeletonParagraphProps {
-    className?: string;
-    style?: CSSProperties;
-    width?: widthUnit | Array<widthUnit>;
-    rows?: number;
+  className?: string;
+  style?: CSSProperties;
+  width?: widthUnit | Array<widthUnit>;
+  rows?: number;
 }
 
-export type SkeletonAvatarProps = Omit<SkeletonElementProps, 'shape'> & DefaultProps & {
+export type SkeletonAvatarProps = Omit<SkeletonElementProps, 'shape'> &
+  DefaultProps & {
     shape?: 'circle' | 'square';
     active?: boolean;
     applyElementStyle?: boolean;
     wrapperStyle?: CSSProperties;
-}
+  };
 
-export type SkeletonImageProps = DefaultProps & Omit<
-    SkeletonElementProps,
-    'size' | 'shape' | 'active'
->;
+export type SkeletonImageProps = DefaultProps &
+  Omit<SkeletonElementProps, 'size' | 'shape' | 'active'>;
 
-export type SkeletonInputProps =  Omit<SkeletonElementProps, 'size' | 'shape'> & DefaultProps & {
+export type SkeletonInputProps = Omit<SkeletonElementProps, 'size' | 'shape'> &
+  DefaultProps & {
     size?: 'large' | 'small' | 'default';
     block?: boolean;
-}
+  };
 
-export type SkeletonButtonProps = Omit<SkeletonElementProps, 'size'> & DefaultProps & {
+export type SkeletonButtonProps = Omit<SkeletonElementProps, 'size'> &
+  DefaultProps & {
     size?: 'large' | 'small' | 'default';
     block?: boolean;
     applyElementStyle?: boolean;
-}
+  };
