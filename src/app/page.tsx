7d43f3dd7d3c9ef0f3164bba06e5c915476a0c40
Gabriel Boyajyan <gabriel.boyajyan.94@gmail.com>
2025-05-17 09:23:39 +0400

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 
+import { ButtonComponent } from "@/index"
+
 
 export default function Home() {
     return (
-        <> </>
+        <>
+            <ButtonComponent>Button</ButtonComponent>
+        </>
     )
 }
