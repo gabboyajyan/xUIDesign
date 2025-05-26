28a1160a918b467a22a1170b945e9b6723c36a95
Gabriel Boyajyan <gabriel.boyajyan.94@gmail.com>
2025-05-26 16:06:41 +0400

    Push command from compile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Checkbox } from "../components/Checkbox";
 export default function Home() {
     return (
         <>
-            <Checkbox onClick={(e) => console.log(e)}>Checkbox</Checkbox>
+            <Checkbox>Checkbox</Checkbox>
         </>
     )
 }
