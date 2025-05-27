9db8802fbed5871d773bdebdbbf81ed00df57f46
Gabriel Boyajyan <gabriel.boyajyan.94@gmail.com>
2025-05-27 16:19:29 +0400

    Push command from compile

--- dist/index.js
+++ dist/index.js
@@ -446,13 +446,13 @@ function styleInject(css, ref) {
 var css_248z$l = ":root{--xui-color-hover:#f5f5f5;--xui-color-disabled:#e6e6e6;--xui-primary-color:#1677ff;--xui-primary-color-light:#40a9ff;--xui-text-color:rgba(0,0,0,.88);--xui-text-color-light:rgba(0,0,0,.5);--xui-error-color:#ff4d4f;--xui-error-color-light:#ff6668;--xui-success-color:#52c41a;--xui-background-color:#fff;--xui-font-size-xs:12px;--xui-font-size-sm:14px;--xui-font-size-md:14px;--xui-font-size-lg:16px;--xui-border-radius-sm:4px;--xui-border-radius-md:4px;--xui-border-radius-lg:6px;--xui-border-color:#d9d9d9;--xui-select-primary-color:var(--xui-primary-color);--xui-select-background-color:var(--xui-background-color)}html{font-family:sans-serif}.globalEllipsis{overflow:hidden;text-overflow:ellipsis;white-space:nowrap}";
 styleInject(css_248z$l);
 
-const ClearIcon = () => /*#__PURE__*/React.createElement("svg", {
+const ClearIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   width: "10",
   height: "10",
   viewBox: "0 0 14 14",
   fill: "none",
   xmlns: "http://www.w3.org/2000/svg"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M1 1L13 13M13 1L1 13",
   stroke: "currentColor",
   strokeWidth: "2",
@@ -460,13 +460,13 @@ const ClearIcon = () => /*#__PURE__*/React.createElement("svg", {
 }));
 const ArrowIcon = ({
   isOpen
-}) => /*#__PURE__*/React.createElement("svg", {
+}) => /*#__PURE__*/React$1.createElement("svg", {
   width: "18",
   height: "18",
   viewBox: "0 0 24 24",
   fill: "none",
   xmlns: "http://www.w3.org/2000/svg"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M7 10L12 15L17 10",
   stroke: "currentColor",
   strokeWidth: "2",
@@ -474,7 +474,7 @@ const ArrowIcon = ({
   strokeLinejoin: "round",
   transform: isOpen ? 'rotate(180, 12, 12)' : ''
 }));
-const LoadingIcon = () => /*#__PURE__*/React.createElement("svg", {
+const LoadingIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   viewBox: "0 0 1024 1024",
   focusable: "false",
   "data-icon": "loading",
@@ -482,10 +482,10 @@ const LoadingIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "12",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"
 }));
-const CheckIcon = () => /*#__PURE__*/React.createElement("svg", {
+const CheckIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   viewBox: "64 64 896 896",
   focusable: "false",
   "data-icon": "check",
@@ -493,10 +493,10 @@ const CheckIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "12",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 00-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"
 }));
-const SearchIcon = () => /*#__PURE__*/React.createElement("svg", {
+const SearchIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   viewBox: "64 64 896 896",
   focusable: "false",
   "data-icon": "search",
@@ -504,10 +504,10 @@ const SearchIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "1em",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"
 }));
-const CalendarIcon = () => /*#__PURE__*/React.createElement("svg", {
+const CalendarIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   viewBox: "64 64 896 896",
   focusable: "false",
   "data-icon": "calendar",
@@ -515,10 +515,10 @@ const CalendarIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "1em",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M880 184H712v-64c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v64H384v-64c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v64H144c-17.7 0-32 14.3-32 32v664c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V216c0-17.7-14.3-32-32-32zm-40 656H184V460h656v380zM184 392V256h128v48c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-48h256v48c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-48h128v136H184z"
 }));
-const SuccessIcon = () => /*#__PURE__*/React.createElement("svg", {
+const SuccessIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   viewBox: "64 64 896 896",
   focusable: "false",
   "data-icon": "check-circle",
@@ -526,10 +526,10 @@ const SuccessIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "1em",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 01-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z"
 }));
-const ErrorIcon = () => /*#__PURE__*/React.createElement("svg", {
+const ErrorIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   className: "error-svg-icon",
   viewBox: "64 64 896 896",
   focusable: "false",
@@ -538,10 +538,10 @@ const ErrorIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "1em",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M512 64c247.4 0 448 200.6 448 448S759.4 960 512 960 64 759.4 64 512 264.6 64 512 64zm127.98 274.82h-.04l-.08.06L512 466.75 384.14 338.88c-.04-.05-.06-.06-.08-.06a.12.12 0 00-.07 0c-.03 0-.05.01-.09.05l-45.02 45.02a.2.2 0 00-.05.09.12.12 0 000 .07v.02a.27.27 0 00.06.06L466.75 512 338.88 639.86c-.05.04-.06.06-.06.08a.12.12 0 000 .07c0 .03.01.05.05.09l45.02 45.02a.2.2 0 00.09.05.12.12 0 00.07 0c.02 0 .04-.01.08-.05L512 557.25l127.86 127.87c.04.04.06.05.08.05a.12.12 0 00.07 0c.03 0 .05-.01.09-.05l45.02-45.02a.2.2 0 00.05-.09.12.12 0 000-.07v-.02a.27.27 0 00-.05-.06L557.25 512l127.87-127.86c.04-.04.05-.06.05-.08a.12.12 0 000-.07c0-.03-.01-.05-.05-.09l-45.02-45.02a.2.2 0 00-.09-.05.12.12 0 00-.07 0z"
 }));
-const DateDistanceIcon = () => /*#__PURE__*/React.createElement("svg", {
+const DateDistanceIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   viewBox: "0 0 1024 1024",
   focusable: "false",
   "data-icon": "swap-right",
@@ -549,10 +549,10 @@ const DateDistanceIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "1em",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M873.1 596.2l-164-208A32 32 0 00684 376h-64.8c-6.7 0-10.4 7.7-6.3 13l144.3 183H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h695.9c26.8 0 41.7-30.8 25.2-51.8z"
 }));
-const TimeIcon = () => /*#__PURE__*/React.createElement("svg", {
+const TimeIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   viewBox: "64 64 896 896",
   focusable: "false",
   "data-icon": "clock-circle",
@@ -560,12 +560,12 @@ const TimeIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "1em",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"
-}), /*#__PURE__*/React.createElement("path", {
+}), /*#__PURE__*/React$1.createElement("path", {
   d: "M686.7 638.6L544.1 535.5V288c0-4.4-3.6-8-8-8H488c-4.4 0-8 3.6-8 8v275.4c0 2.6 1.2 5 3.3 6.5l165.4 120.6c3.6 2.6 8.6 1.8 11.2-1.7l28.6-39c2.6-3.7 1.8-8.7-1.8-11.2z"
 }));
-const StampleIcon = () => /*#__PURE__*/React.createElement("svg", {
+const StampleIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   viewBox: "64 64 896 896",
   focusable: "false",
   "data-icon": "paper-clip",
@@ -573,10 +573,10 @@ const StampleIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "1em",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M779.3 196.6c-94.2-94.2-247.6-94.2-341.7 0l-261 260.8c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0012.7 0l261-260.8c32.4-32.4 75.5-50.2 121.3-50.2s88.9 17.8 121.2 50.2c32.4 32.4 50.2 75.5 50.2 121.2 0 45.8-17.8 88.8-50.2 121.2l-266 265.9-43.1 43.1c-40.3 40.3-105.8 40.3-146.1 0-19.5-19.5-30.2-45.4-30.2-73s10.7-53.5 30.2-73l263.9-263.8c6.7-6.6 15.5-10.3 24.9-10.3h.1c9.4 0 18.1 3.7 24.7 10.3 6.7 6.7 10.3 15.5 10.3 24.9 0 9.3-3.7 18.1-10.3 24.7L372.4 653c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0012.7 0l215.6-215.6c19.9-19.9 30.8-46.3 30.8-74.4s-11-54.6-30.8-74.4c-41.1-41.1-107.9-41-149 0L463 364 224.8 602.1A172.22 172.22 0 00174 724.8c0 46.3 18.1 89.8 50.8 122.5 33.9 33.8 78.3 50.7 122.7 50.7 44.4 0 88.8-16.9 122.6-50.7l309.2-309C824.8 492.7 850 432 850 367.5c.1-64.6-25.1-125.3-70.7-170.9z"
 }));
-const TrashIcon = () => /*#__PURE__*/React.createElement("svg", {
+const TrashIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   viewBox: "64 64 896 896",
   focusable: "false",
   "data-icon": "delete",
@@ -584,10 +584,10 @@ const TrashIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "1em",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M360 184h-8c4.4 0 8-3.6 8-8v8h304v-8c0 4.4 3.6 8 8 8h-8v72h72v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80h72v-72zm504 72H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zM731.3 840H292.7l-24.2-512h487l-24.2 512z"
 }));
-const SpinerIcon = () => /*#__PURE__*/React.createElement("svg", {
+const SpinerIcon = () => /*#__PURE__*/React$1.createElement("svg", {
   viewBox: "0 0 1024 1024",
   focusable: "false",
   "data-icon": "loading",
@@ -595,7 +595,7 @@ const SpinerIcon = () => /*#__PURE__*/React.createElement("svg", {
   height: "1em",
   fill: "currentColor",
   "aria-hidden": "true"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/React$1.createElement("path", {
   d: "M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"
 }));
 
--- lib/components/Icons/Icons.tsx
+++ lib/components/Icons/Icons.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const ClearIcon = () => (
   <svg
     width="10"
