1161cb55fc16f4b0c914ef9bae97ceea81075c79
Gabriel Boyajyan <gabriel.boyajyan.94@gmail.com>
2025-09-09 13:47:36 +0400

    Push command from compile

--- dist/index.js
+++ dist/index.js
@@ -462,9 +462,9 @@ const ClearIcon = () => /*#__PURE__*/React.createElement("svg", {
 const ArrowIcon = ({
   isOpen
 }) => /*#__PURE__*/React.createElement("svg", {
-  width: "16",
-  height: "16",
-  viewBox: "0 0 18 22",
+  width: "20",
+  height: "20",
+  viewBox: "0 0 24 24",
   fill: "none",
   xmlns: "http://www.w3.org/2000/svg"
 }, /*#__PURE__*/React.createElement("path", {
--- lib/components/Icons/Icons.tsx
+++ lib/components/Icons/Icons.tsx
@@ -19,9 +19,9 @@ const ClearIcon = () => (
 
 const ArrowIcon = ({ isOpen }: { isOpen: boolean }) => (
   <svg
-    width="16"
-    height="16"
-    viewBox="0 0 18 22"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
