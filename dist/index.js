3a3b219413a2af6c04027baf5082e12a99719aba
Gabriel Boyajyan <gabriel.boyajyan.94@gmail.com>
2025-11-04 18:46:24 +0400

    Push command from compile

--- dist/index.js
+++ dist/index.js
@@ -2130,7 +2130,7 @@ const DatePicker = ({
     }
     if (typeof format === 'string') {
       date = new Date(date);
-      return format.replace(/YYYY/, date.getFullYear().toString()).replace(/MMM/, monthNames[date.getMonth() + 1]).replace(/MM/, (date.getMonth() + 1).toString().padStart(2, '0')).replace(/DD/, date.getDate().toString().padStart(2, '0'));
+      return format.replace(/YYYY/, date.getFullYear().toString()).replace(/MMM/, monthNames[date.getMonth()]).replace(/MM/, (date.getMonth() + 1).toString().padStart(2, '0')).replace(/DD/, date.getDate().toString().padStart(2, '0'));
     }
     return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
   }
--- lib/components/DatePicker/DatePicker.tsx
+++ lib/components/DatePicker/DatePicker.tsx
@@ -165,7 +165,7 @@ const DatePicker = ({
 
       return format
         .replace(/YYYY/, date.getFullYear().toString())
-        .replace(/MMM/, monthNames[(date.getMonth() + 1)])
+        .replace(/MMM/, monthNames[date.getMonth()])
         .replace(/MM/, (date.getMonth() + 1).toString().padStart(2, '0'))
         .replace(/DD/, date.getDate().toString().padStart(2, '0'));
     }
