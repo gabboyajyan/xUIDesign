bdbcd973d9a37fb352f90a322c6b2a035473b074
Gabriel Boyajyan <gabriel.boyajyan.94@gmail.com>
2025-12-02 13:13:49 +0400

    Push command from compile

--- lib/hooks/usePosition.ts
+++ lib/hooks/usePosition.ts
@@ -99,7 +99,7 @@ export const usePosition = ({
                     : (inputRect.left || 0) + document.documentElement.scrollLeft
 
             const _top = isRight || isLeft
-                ? triggerRef.current.scrollTop
+                ? (inputRect.top || 0) + document.documentElement.scrollTop
                 : (inputRect.top || 0) + document.documentElement.scrollTop;
 
             setDropdownPosition({
