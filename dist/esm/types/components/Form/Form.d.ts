64428cf0ff88faab75114d324d3dfce2ee5ab2cf
Gabriel Boyajyan <gabriel.boyajyan.94@gmail.com>
2026-02-03 16:59:40 +0400

    Push command from compile

--- dist/esm/types/components/Form/Form.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react';
-import { FormInstance, FormItemProps, FormProps } from '../../types/form';
-export declare const FormContext: React.Context<FormInstance | null>;
-declare const Form: FC<FormProps> & {
-    Item: FC<FormItemProps>;
-};
-export default Form;
