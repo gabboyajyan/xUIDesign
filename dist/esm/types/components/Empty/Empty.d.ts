838469cc806a30127c55cb1590f06da62c1f7275
Gabriel Boyajyan <gabriel.boyajyan.94@gmail.com>
2026-02-03 12:11:52 +0400

    Push command from compile

--- dist/esm/types/components/Empty/Empty.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { EmptyContentProps } from '../../types/empty';
-import './style.css';
-declare const EmptyContent: ({ icon, style, className, title, description, prefixCls, prefixClsV3 }: EmptyContentProps) => React.JSX.Element;
-export default EmptyContent;
